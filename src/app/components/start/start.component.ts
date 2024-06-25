e774e78f058ac1bec8609f2d1f4284ce5b9ced29
Jcabpri287 <jcabpri287@g.educaand.es>
2024-06-25 20:01:26 +0200

    Añadida mi foto y cambios menores

---
+++ src/app/components/start/start.component.ts
@@ -0,0 +1,14 @@
+import * as THREE from 'three';
+import { Component, AfterViewInit, OnDestroy, Renderer2 } from '@angular/core';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+@Component({
+  selector: 'app-start',
+  standalone: true,
+  imports: [],
+  templateUrl: './start.component.html',
+  styleUrl: './start.component.scss'
+})
+export class StartComponent{
+  
+}
